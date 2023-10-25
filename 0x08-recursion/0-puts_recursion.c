31541ca6c37624c6d07a38167cf29d706df9f42f
blaccsuperman <dialonzokala@yahoo.com>
2023-10-25 10:17:35 -0700

    recursion complete

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _puts_recursion - print a given string 
+ * _puts_recursion - print a given string
  *
  * @s: the string accepted
  * Description: The code below prints string
  * using putchar character by character
- * Return: 0
+ * Return: (0)
  *
  */
 
@@ -13,12 +13,12 @@ void _puts_recursion(char *s)
 {
 	unsigned int count = 0;
 
-	if(s[count] == '\0')
+	if (s[count] == '\0')
 	{
 		_putchar('\n')
-		return 0;
+		return (0);
 	}
 	_putchar(s[count]);
-	_puts_recursion(s+1);
+	_puts_recursion(s + 1);
 }
 
