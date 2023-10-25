4dd9fde7ebdec384c212738fad8175e09afab7bb
blaccsuperman <dialonzokala@yahoo.com>
2023-10-25 16:11:19 -0700

    reverse string

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 
-/**
- *
+/** 
  * _print_rev_recursion - Prints a string in reverse.
  *
- * @s: The string to be printed.
  * Description : prints a reverse of a string
+ * @s: The string to be printed.
  * Return : 0
  */
 void _print_rev_recursion(char *s)
