8332b2039af308ade4c2dae15014ce56acd3899a
blaccsuperman <dialonzokala@yahoo.com>
2023-10-25 17:37:42 -0700

    sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns square root of n
+ * @n: number whose square root is solved
+ * Return: squareroot of n
+ */
+
+
+/**
+ * halp - helper function to find sqrt of n
+ * @b: number to check if sqrt
+ * @c: iteration count
+ * Return: sqrt if natural
+ */
+int hefu(int b, int c)
+{
+	int sq;
+
+	sq = c * c;
+	if (sq == b)
+		return (c);
+	else if (sq < b)
+		return (hefu(b, c + 1));
+	else
+		return (-1);
+}
+int _sqrt_recursion(int n)
+{
+	return (hefu(n, 1));
+}
