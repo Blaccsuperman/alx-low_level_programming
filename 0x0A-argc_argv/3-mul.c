277f53f143cf83154c7c6ae2f3391cdfe7435a7f
blaccsuperman <dialonzokala@yahoo.com>
2023-10-27 11:56:50 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,8 +13,8 @@ int main(int argc, char *argv[])
 
 	if (argc == 3)
 	{
-		n1 = atoi (argv[1]);
-		n2 = atoi (argv[2]);
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
 		mult = n1 * n2;
 		printf("%d\n", mult);
 		return (0);
