50325d373c9249971b7b495a757ba1f11a7da291
blaccsuperman <dialonzokala@yahoo.com>
2023-11-07 07:45:00 -0800

    0-poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct dog with elements:
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: doggos owner's name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
